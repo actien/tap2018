9ad27a9e2c1339f18d7084a691790546834e8ca2
Andy Tien <ac.tien@gmail.com>
2018-04-21 21:30:06 -0400

    Update copyright

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'http://tap-ny.org/', 'tap-ny' ) ); ?>"><?php printf( 'TAP-NY | Taiwanese American Professionals - New York'); ?></a>
+<p>
+  <br/>
+  &copy; 2017 TAP-NY. All rights reserved.
+  A 501(c)(3) organization for the Taiwanese American community in the NYC area.
+</p>
+</div><!-- .site-info -->
